f1f25339da0e11e6d73997fefd3aab4abb5062ae
DmitryYug <dmitriyyuganuk@gmail.com>
2023-06-04 11:32:40 +0300

    policy route

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,11 +1,13 @@
 import {Route, Routes} from "react-router-dom";
 import {CasesPage} from "../components/blocks/CasesPage";
 import App from "../App";
+import {PolicyPage} from "./blocks/PolicyPage";
 
 export function AppRouter() {
     return (
         <Routes>
             <Route path={'/nt-dev-landing-lorem'} element={<App/>}/>
+            <Route path={'/policy'} element={<PolicyPage/>}/>
             <Route path={'/:projectId'} element={<CasesPage/>}/>
         </Routes>
     );
